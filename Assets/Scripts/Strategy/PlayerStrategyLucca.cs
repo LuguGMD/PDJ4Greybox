985ef5bdaa24210e52ec1a36382c51ff539bcb74
lucca100S <luccacatalan@gmail.com>
2025-09-27 12:58:39 -0300

    feat: dash strategy

--- Assets/Scripts/Strategy/PlayerStrategyLucca.cs
+++ Assets/Scripts/Strategy/PlayerStrategyLucca.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace Player.Strategy
@@ -5,6 +6,23 @@ namespace Player.Strategy
     //[CreateAssetMenu(fileName = "PlayerStrategyLucca", menuName = "Scriptable Objects/PlayerStrategyLucca")]
     public class PlayerStrategyLucca : PlayerStrategyScriptable
     {
+        [SerializeField] float _dashDistance = 3;
+
+        [SerializeField] float _dashDuration = 0.15f;
+        [SerializeField] float _dashCooldown = 0.5f;
+
+        float dashTimer;
+        Vector3 dashDirection;
+        float dashSpeed;
+        float dashRotationSpeed = 3000;
+
+        #region Properties
+        public float dashDistance { get { return _dashDistance; } private set { _dashDistance = value; } }
+        public float dashDuration { get { return _dashDuration; } private set { _dashDuration = value; } }
+        public float dashCooldown { get { return _dashCooldown; } private set { _dashCooldown = value; } }
+
+        #endregion
+
         private PlayerStrategyHandler.Strategy m_strategy = PlayerStrategyHandler.Strategy.Lucca;
         public override PlayerStrategyHandler.Strategy strategy { get => m_strategy; protected set { m_strategy = value; } }
 
@@ -14,7 +32,18 @@ namespace Player.Strategy
         }
         public override void Move(PlayerMovement player)
         {
-            base.Move(player);
+            if (dashTimer < dashDuration)
+            {
+                player.force = dashSpeed * dashDirection;
+            }
+            else
+            {
+                Vector3 movement = player.direction * _speed;
+                movement.y = _gravity;
+
+                player.force = new Vector3(movement.x, player.force.y, movement.z);
+            }
+            dashTimer += Time.deltaTime;
         }
         public override void GetDirection(PlayerMovement player)
         {
@@ -23,15 +52,30 @@ namespace Player.Strategy
         public override void Rotate(PlayerMovement player)
         {
             base.Rotate(player);
+
+            if (dashTimer < dashDuration)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(dashDirection, Vector3.up);
+                toRotation = Quaternion.Euler(0, toRotation.eulerAngles.y, 0);
+
+                player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation, toRotation,  dashRotationSpeed * Time.deltaTime);
+            }
         }
         public override void Skill(PlayerMovement player)
         {
-            base.Skill(player);
+            if (dashTimer >= dashDuration + dashCooldown)
+            {
+                dashTimer = 0;
+                dashDirection = player.direction.magnitude == 0 ? Camera.main.transform.forward : player.direction;
+                dashDirection.y = 0;
+                dashDirection.Normalize();
+            }
         }
 
         public override void EnterStrategy(PlayerMovement player)
         {
-            base.EnterStrategy(player);
+            dashTimer = dashDuration + dashCooldown;
+            dashSpeed = dashDistance / dashDuration;
         }
 
         public override void UpdateStrategy(PlayerMovement player)
